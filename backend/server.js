d38240cf6bd8883a64f6089d0ff9779d111dd180
Sai <lakshmisai55700@gmail.com>
2025-11-10 17:27:30 +0530

    latest

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,10 @@ app.use(rateLimit({
 const WHITELIST = new Set([
   "https://visualstech.in",
   "http://localhost:5173",
+  "https://visualstechfrontend.onrender.com",
   "http://127.0.0.1:5173",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000"
+  "http://localhost:5000",
+  "http://127.0.0.1:5000"
 ]);
 
 // CORS middleware with dynamic origin handling
